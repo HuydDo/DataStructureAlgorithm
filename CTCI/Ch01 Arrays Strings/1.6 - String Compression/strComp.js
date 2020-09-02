c62b3e477af4b9562a57279737b71cbea0e9785a
HuydDo <huydinhdo@yahoo.com>
2020-09-02 12:23:05 -0600

    modified strComp

--- CTCI/Ch01 Arrays Strings/1.6 - String Compression/strComp.js
+++ CTCI/Ch01 Arrays Strings/1.6 - String Compression/strComp.js
@@ -1,21 +1,28 @@
 function strComp(string){
-  let obj = {}
-  for(let i = 0; i < string.length; i++){
-    if (obj[string.charAt(i)]){
-      obj[string.charAt(i)] +=1
-    } else {
-      obj[string.charAt(i)] =1
-    }
+  let str = string.toLowerCase()
+  let arr = []
+  let count = 1
 
-    for (let prop in obj){
-      // console.log(prop + obj[prop])
-      return (prop + obj[prop])
+  for(let i = 0; i < str.length; i++){
+
+    let currChar = str[i]
+    if(str[i] === string[i+1]){
+      count++
+    } else {
+      arr.push(currChar, count)
+      count = 1
     }
   }
-
+  // console.log('arr.len:' + arr.length + ' str.len: ' + str.length)
+  if(arr.length > str.length){
+    return str
+  } else {
+    return arr.join('')
+  }
+    
 }
 
 
 // Test
-// console.log('aaaaaa', strComp('aaaaaa'), 'a6');
+console.log('aaaaaa', strComp('aaaaaa'), 'a6');
 console.log('aabcccccaaa', strComp('aabcccccaaa'), 'a2b1c5a3');
