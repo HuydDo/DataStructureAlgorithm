8266d1ee37f0d0d5ed4c7a238b0511f615f8e714
HuydDo <huydinhdo@yahoo.com>
2020-09-03 16:49:43 -0600

    added hasPairWithSum

---
+++ Practice/hasPairWithSum.js
@@ -0,0 +1,19 @@
+function hasPairWithSum(arr, sum){
+  let set = new Set() //empty complements set
+
+  for (let num of arr){
+    if(set.has(num) ){
+      return true
+    } else {
+      set.add(sum - num) //insert complement = 8 - num; eg: 8-1=7
+    }
+  }
+  return false
+}
+
+let arr1 = [1,2,3,9] //false
+let arr2 = [2,1,4,4] //true because pair 4,4 = 8
+let arr3 = [] //false
+console.log(hasPairWithSum(arr1, 8))
+console.log(hasPairWithSum(arr2, 8))
+console.log(hasPairWithSum(arr3, 8))
