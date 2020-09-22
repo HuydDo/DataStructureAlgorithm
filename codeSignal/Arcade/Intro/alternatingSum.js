d4e29f121befeef9dd02eb92c01343982955a8ad
HuydDo <huydinhdo@yahoo.com>
2020-09-21 22:10:24 -0600

    modified alternatingSums

--- codeSignal/Arcade/Intro/alternatingSum.js
+++ codeSignal/Arcade/Intro/alternatingSum.js
@@ -1,11 +1,10 @@
 function alternatingSums(a) {
   let p = [0,0]
   for (let i in a) {
-    console.log("i = " + i + " i % 2 = " + i % 2);
+    // console.log("i = " + i + " i % 2 = " + i % 2);
     p[i % 2 == 0 ? 0 : 1] += a[i];
     }
     return p
-  
 }
 
 
