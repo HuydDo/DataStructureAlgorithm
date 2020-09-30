de888e31bab0ff1d5d12840cca604fa368dce35a
HuydDo <huydinhdo@yahoo.com>
2020-09-30 01:02:54 -0600

    modified mergeSortedArray

--- Practice/mergeSortedArray.js
+++ Practice/mergeSortedArray.js
@@ -5,24 +5,36 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-// var merge = function(nums1, m, nums2, n) {
   function mergeSortedArray(nums1, m, nums2, n) {
-  let length = m + n - 1
-  m--
-  n--
-  while (n >= 0) {
-    if (nums1[m] > nums2[n]) {
-      nums1[length--] = nums1[m--]
+  let i = m -1
+  let j = n - 1
+  let k = m + n - 1
+
+  while (i >= 0 && j >= 0 ) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i]
+      k--;
+      i--;
     } else {
-      nums1[length--] = nums2[n--] 
+      nums1[k] = nums2[j] 
+      k--;
+      j--;
     }
+    console.log(nums1)
   }
-  return nums1
+  while(j >= 0){
+    nums1[k] = nums2[j];
+    k--;
+    j--;
+    console.log(nums1)
+  }
+
+  console.log("\t -> mergeSortedArray:", nums1);
 }
 
 //Input:
 let nums1 = [1,2,3,0,0,0], m = 3
 let nums2 = [2,5,6], n = 3
 
-console.log(merge(nums1, m, nums2, n))
+mergeSortedArray(nums1, m, nums2, n)
 //Output: [1,2,2,3,5,6]
