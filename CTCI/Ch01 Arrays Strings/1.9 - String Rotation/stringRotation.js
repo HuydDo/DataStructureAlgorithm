0bcfc3a44e46b95c8094954c176a185d305ad2da
HuydDo <huydinhdo@yahoo.com>
2020-10-04 16:16:29 -0600

    modified stringRotation.js

--- CTCI/Ch01 Arrays Strings/1.9 - String Rotation/stringRotation.js
+++ CTCI/Ch01 Arrays Strings/1.9 - String Rotation/stringRotation.js
@@ -1,3 +1,10 @@
 var stringRotation = function(string1, string2){
-  
+  if (string1.length !== string2.length){
+    return false;
+  }
+  return (string2 + string1).includes(string1)
+
 }
+
+// Test
+console.log(stringRotation('waterbottle', 'erbottlewat'), true);
