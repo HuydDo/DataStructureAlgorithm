e86e37a4e9947242680cd945b2a6bc8c2c6e0099
HuydDo <huydinhdo@yahoo.com>
2020-10-04 16:18:21 -0600

    modified stringRotation.js

--- CTCI/Ch01 Arrays Strings/1.9 - String Rotation/stringRotation.js
+++ CTCI/Ch01 Arrays Strings/1.9 - String Rotation/stringRotation.js
@@ -2,7 +2,7 @@ var stringRotation = function(string1, string2){
   if (string1.length !== string2.length){
     return false;
   }
-  return (string2 + string1).includes(string1)
+  return (string2 + string2).includes(string1)
 
 }
 
