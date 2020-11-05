67da8b6aef0a27d90cf2a21239cfe184bfa4b50f
HuydDo <huydinhdo@yahoo.com>
2020-11-04 17:08:20 -0700

    add removeDuplicate.js

---
+++ leetCode/hashTable/array/removeDuplicate.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * Time complextiy : O(n). Assume that n is the length of array. Each of i and j traverses at most n steps.
+
+Space complexity : O(1).
+ */
+var removeDuplicates = function(nums) {
+    if (nums.length == 0) return 0;
+    let i = 0;
+    let newArray = []
+    for(let j=1; j<nums.length; j++) {
+      if (nums[i] != nums[j]){
+        i++;
+        nums[i] = nums[j];
+        newArray[i] = nums[j]
+      }
+    }
+    console.log(nums.slice(0,newArray.length))
+    return i+1
+};
+
+let nums = [1,1,2]
+// let nums = [0,0,1,1,1,2,2,3,3,4]
+console.log(removeDuplicates(nums))
