bd10a4efa20226f96089b8f2e91b3e4d493c6405
HuydDo <huydinhdo@yahoo.com>
2020-11-06 10:09:22 -0700

    modified checkIfExist.js

--- leetCode/hashTable/array/checkIfExist.js
+++ leetCode/hashTable/array/checkIfExist.js
@@ -2,19 +2,30 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-var checkIfExist = function(arr) {
-    for(let i=0; i<arr.length; i++){
-      let num = arr[i]
-      for (let j=0; j <arr.length; j++){
-        if (j !== i){
-         if (arr[j] === num * 2){
-           return true
-         }
-        }
-      }
+// var checkIfExist = function(arr) {
+//     for(let i=0; i<arr.length; i++){
+//       let num = arr[i]
+//       for (let j=0; j <arr.length; j++){
+//         if (j !== i){
+//          if (arr[j] === num * 2){
+//            return true
+//          }
+//         }
+//       }
+//     }
+//     return false;
+// };
+
+var checkIfExist = function(arr){
+  let set = new Set();
+  for (let i=0; i <arr.length; i++){
+    if (set.has(arr[i] * 2) || set.has(arr[i] / 2)){
+      return true
     }
-    return false;
-};
+    set.add(arr[i])
+  }
+  return false
+}
 
 
 // let arr = [10,2,5,3] //true
