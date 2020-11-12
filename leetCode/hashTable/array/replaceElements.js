fb42a76cb84fadd78e589850edd7f7876d5ac684
HuydDo <huydinhdo@yahoo.com>
2020-11-12 16:36:16 -0700

    modified replaceElements.js

--- leetCode/hashTable/array/replaceElements.js
+++ leetCode/hashTable/array/replaceElements.js
@@ -2,6 +2,18 @@
  * @param {number[]} arr
  * @return {number[]}
  */
-var replaceElements = function(arr) {
+// var replaceElements = function(arr) {
     
+// };
+
+const replaceElements = arr => {
+  for (let i = arr.length - 2, tmp = arr[arr.length - 1]; 0 <= i; i--)
+    if (tmp < arr[i]) [arr[i], tmp] = [tmp, arr[i]];
+    else arr[i] = tmp;
+  arr[arr.length - 1] = -1;
+  return arr;
 };
+
+
+let arr = [17,18,5,4,6,1] //Output: [18,6,6,6,1,-1]
+console.log(replaceElements(arr))
