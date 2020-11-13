4e1a466400dc8868f6e82d5af70b472e41d782f2
HuydDo <huydinhdo@yahoo.com>
2020-11-13 11:39:43 -0700

    added sortArrayBYParity.js

---
+++ leetCode/hashTable/array/sortArrayByParity.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortArrayByParity = function(A) {
+    
+};
+
+let A = [3,1,2,4] //Output: [2,4,3,1]
+
+console.log(sortArrayByParity(A))
