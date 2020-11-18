a0281289d6bff1ee254d1ad7f4a5a4d3b5ef425a
HuydDo <huydinhdo@yahoo.com>
2020-11-17 21:16:08 -0700

    modified designLinkedList.js

--- leetCode/linkedList/designLinkedList.js
+++ leetCode/linkedList/designLinkedList.js
@@ -9,6 +9,29 @@ var ListNode = function(val) {
   this.next = null
 };
 
+/**
+ * Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+ * @param {number} index
+ * @return {number}
+ */
+MyLinkedList.prototype.get = function(index) {
+  if (index<0){
+    return -1
+  }
+  var cur = this.dummy.next
+  while(cur && index>0){
+    cur = cur.next
+    index--
+  }
+  if (!cur){
+    return -1
+  }
+  return cur.val
+}
+
+
+
+
 
 /**
 * Your MyLinkedList object will be instantiated and called as such:
@@ -19,5 +42,18 @@ var ListNode = function(val) {
 * obj.addAtIndex(index,val)
 * obj.deleteAtIndex(index)
 */
-["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"]
-[[], [1], [3], [1, 2], [1], [1], [1]]
+
+// ["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"]
+// [[], [1], [3], [1, 2], [1], [1], [1]]
+
+
+// Explanation
+// MyLinkedList myLinkedList = new MyLinkedList();
+// myLinkedList.addAtHead(1);
+// myLinkedList.addAtTail(3);
+// myLinkedList.addAtIndex(1, 2);    // linked list becomes 1->2->3
+// myLinkedList.get(1);              // return 2
+// myLinkedList.deleteAtIndex(1);    // now the linked list is 1->3
+// myLinkedList.get(1);              // return 3
+
+var obj = new MyLinkedList()
