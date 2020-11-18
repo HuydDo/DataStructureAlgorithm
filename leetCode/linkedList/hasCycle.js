68a6b8f9a8b3f3f970c242d5b83d036c3c9d15c6
HuydDo <huydinhdo@yahoo.com>
2020-11-18 16:16:13 -0700

    modified hasCycle.js

--- leetCode/linkedList/hasCycle.js
+++ leetCode/linkedList/hasCycle.js
@@ -11,5 +11,17 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    
+ if (!head || !head.next){
+   return false
+ }
+ var slow = head
+ var fast = head.next
+ while(fast && fast.next){
+   if(fast === slow){
+     return true
+   }
+   slow = slow.next
+   fast = fast.next.next
+ }
+ return false
 };
