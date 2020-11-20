f56c91ca52ababb3002793f849dd7ce007eff529
HuydDo <huydinhdo@yahoo.com>
2020-11-19 22:46:50 -0700

    modifed removeDups.js

--- CTCI/Ch02 Linked List/2.1 - Remove Dups/removeDups.js
+++ CTCI/Ch02 Linked List/2.1 - Remove Dups/removeDups.js
@@ -15,3 +15,39 @@ var checkDups = function(head, node){
   }
   return false;
 };
+
+var printLinkedList = function(head){
+  var node = head;
+  console.log('start of linked list');
+  while(node !== null){
+    console.log(node.value);
+    node = node.next;
+  }
+  console.log('end of linked list')
+}
+
+var removeDups = function(head){
+  var node = head;
+  while(node !== null){
+    if (node.next !== null && checkDups(head, node.next)){
+      node.next = node.next.next;
+    } else {
+      node = node.next;
+    }
+  }
+  return head;;
+}
+/* TEST */
+var a = new LinkedList('a');
+var b = new LinkedList('b');
+var c = new LinkedList('c');
+var d = new LinkedList('d');
+var e = new LinkedList('e');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+removeDups(a);
+printLinkedList(a);
