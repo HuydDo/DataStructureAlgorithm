0bb439b990bc70c6be5f28010074805c6a7ee05c
HuydDo <huydinhdo@yahoo.com>
2020-11-24 22:50:26 -0700

    modified retrunKthToLast.js

--- CTCI/Ch02 Linked List/2.2 - Return Kth to Last/returnKthToLast.js
+++ CTCI/Ch02 Linked List/2.2 - Return Kth to Last/returnKthToLast.js
@@ -2,3 +2,16 @@ var linkedList = function(value){
   this.value = value;
   this.next = null
 }
+
+var findKthToLast = function(k,head){
+  //do recursively
+  if (head === null || k<1){
+    return;
+  } else if(k===1){
+    console.log(head.value);
+    findKthToLast(k, head.next);
+  } else {
+    findKthToLast(k-1, head.next);
+  }
+}
+
