03c8a619bb1bd1378e10307a55aa0d2be67f8491
HuydDo <huydinhdo@yahoo.com>
2020-11-25 23:57:03 -0700

    modified detectCycle.js

--- leetCode/linkedList/detectCycle.js
+++ leetCode/linkedList/detectCycle.js
@@ -11,5 +11,22 @@
  * @return {ListNode}
  */
 var detectCycle = function(head) {
-    
+  var slow = head
+  var fast = head
+  while (fast && fast.next) {
+      slow = slow.next
+      fast = fast.next.next
+      if (slow === fast) {
+          break;
+      }
+  }
+  if (!fast || !fast.next) {
+      return null
+  }
+  slow = head
+  while (slow !== fast) {
+      slow = slow.next
+      fast = fast.next
+  }
+  return slow
 };
