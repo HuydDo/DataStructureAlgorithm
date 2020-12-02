51e622e936d8bd70030022c5c181bf460da04d3f
HuydDo <huydinhdo@yahoo.com>
2020-12-02 14:27:53 -0700

    modified deleteMiddleNode.js

--- CTCI/Ch02 Linked List/2.3 - Delete Middle Node/deleteMiddleNode.js
+++ CTCI/Ch02 Linked List/2.3 - Delete Middle Node/deleteMiddleNode.js
@@ -2,3 +2,14 @@ var LinkedList = function(value){
   this.value = value;
   this.next = null;
 }
+
+var deleleMidNode = function(midNode){
+  var node = midNode;
+  while (node !== null && node.next !== null) {
+    node.value = node.next.value;
+    if (node.next.next === null) {
+      node.next = null;
+    } 
+    node = node.next;
+  }
+}
