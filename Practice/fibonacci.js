8ba602333bcc57b4443f03a70d77aab20f4db980
HuydDo <huydinhdo@yahoo.com>
2020-12-05 16:18:32 -0700

    modified fibonacci.js

--- Practice/fibonacci.js
+++ Practice/fibonacci.js
@@ -1,6 +1,6 @@
 const fibonacci = function(number) {
   if (number <= 1) {
-    console.log(number)
+    // console.log(number)
     return number;
   }
   return fibonacci(number - 2) + fibonacci(number - 1);
@@ -11,3 +11,26 @@ console.log(fibonacci(4))  //3
 //2 + 3
 //2 => 0 + 1 => 0 + 1
 //3 => 1 + 2 => 1 + 0 + 1
+
+
+function fibonacci2(n) {  
+  const fibSequence = [1];   
+  let currentValue = 1;  
+  let previousValue = 0;   
+  if (n === 1) { 
+     return fibSequence;  
+  }   
+  let iterationsCounter = n - 1;   
+  while (iterationsCounter) { 
+    console.log('counter ',iterationsCounter)   
+    currentValue += previousValue;   
+    console.log('currVal ', currentValue) 
+    previousValue = currentValue - previousValue;
+    console.log('preVal ', previousValue)
+    fibSequence.push(currentValue);     
+    iterationsCounter -= 1;  
+  }   
+  return fibSequence;
+}
+
+console.log(fibonacci2(3))  //[1, 1, 2]
