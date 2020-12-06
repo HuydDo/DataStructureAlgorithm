2cfa2361b1fb075ec7061c545ce63ae2db1ddd7d
HuydDo <huydinhdo@yahoo.com>
2020-12-05 22:26:31 -0700

    modified reverseList.js

--- leetCode/linkedList/reverseList.js
+++ leetCode/linkedList/reverseList.js
@@ -10,5 +10,13 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    
-};
+  let prev = null;
+  let curr = head;
+  while (curr != null) {
+      let nextTemp = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = nextTemp;
+  }
+  return prev;
+}
