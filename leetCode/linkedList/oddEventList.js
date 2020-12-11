79409f4b3f4400db57f54b79cd7d5d914e87ee85
HuydDo <huydinhdo@yahoo.com>
2020-12-11 13:08:15 -0700

    modified oddEventList.js

--- leetCode/linkedList/oddEventList.js
+++ leetCode/linkedList/oddEventList.js
@@ -10,5 +10,14 @@
  * @return {ListNode}
  */
 var oddEvenList = function(head) {
-    
+  if (head == null) return null;
+     let odd = head, even = head.next, evenHead = even;
+      while (even != null && even.next != null) {
+          odd.next = even.next;
+          odd = odd.next;
+          even.next = odd.next;
+          even = even.next;
+      }
+      odd.next = evenHead;
+      return head;
 };
