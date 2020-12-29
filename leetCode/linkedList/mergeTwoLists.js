b7b3c0ca7dc61f835a212dc7658e71ff7afea5b3
HuydDo <huydinhdo@yahoo.com>
2020-12-29 01:06:59 -0700

    modified mergeTwoList.js

--- leetCode/linkedList/mergeTwoLists.js
+++ leetCode/linkedList/mergeTwoLists.js
@@ -11,5 +11,22 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    
+  let dummy = new ListNode(0)
+  var cur = dummy
+  while (l1 && l2) {
+      if (l1.val < l2.val) {
+          cur.next = l1
+          l1 = l1.next
+      } else {
+          cur.next = l2
+          l2 = l2.next
+      }
+      cur = cur.next
+  }
+  if (!l1) {
+      cur.next = l2
+  } else {
+      cur.next = l1
+  }
+  return dummy.next
 };
