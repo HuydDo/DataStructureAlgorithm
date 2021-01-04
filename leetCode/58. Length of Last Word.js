2888539bdaf21e80752f3fd81dda153eae110071
HuydDo <huydinhdo@yahoo.com>
2021-01-03 21:50:23 -0700

    modified 58. Lenght of Last Word.js

--- leetCode/58. Length of Last Word.js
+++ leetCode/58. Length of Last Word.js
@@ -3,5 +3,18 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-  
+  let lastWordLen = 0
+  let currWordLen = 0
+  for(let i = 0; i < s.length; i++){
+    if (s.charAt(i) == ' ')
+      currWordLen = 0
+    else{
+      currWordLen++
+      lastWordLen = currWordLen
+    }
+  }
+  return lastWordLen 
 };
+
+console.log(lengthOfLastWord("Hello World"))
+console.log(lengthOfLastWord(" "))
