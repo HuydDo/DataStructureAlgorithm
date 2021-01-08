7b1b83acc2a5615af2f2d5bc01beb15a7531c83b
HuydDo <huydinhdo@yahoo.com>
2021-01-07 23:45:25 -0700

    modified method 2

--- leetCode/linkedList/addTwoNumbers.js
+++ leetCode/linkedList/addTwoNumbers.js
@@ -43,12 +43,33 @@ var addTwoNumbers = function(l1, l2) {
 
 /*Method #2 */
 var addTwoNumbers = function(l1, l2) {
-  let cur1 = l1
-  let cur2 = l2
-  let dummy = new ListNode(0)
-  let curr = dummy
-  let carry = 0
+  let cur1 = l1;
+  let cur2 = l2;
+  let dummy = new ListNode(0);
+  let cur = dummy;
+  let carry = 0;
 
+  while (cur1 !== null || cur2 !== null){
+    let x = (cur1 !== null) ? cur1.val : 0;
+    let y = (cur2 !== null) ? cur2.val : 0;
+    let sum = x + y + carry;
 
+    carry = Math.floor(sum / 10);
+    cur.next = new ListNode(sum % 10);
+    cur = cur.next;
+  
+    if(cur1 !== null){
+      cur1 = cur1.next;
+    }
 
+    if(cur2 !== null){
+      cur2 = cur2.next;
+    }
+  }
+
+  if (carry > 0){
+    cur.next = new ListNode(1);
+  }
+
+  return dummy.next;
 };
