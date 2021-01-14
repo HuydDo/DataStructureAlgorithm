5919b19c43cd4b8209e564c0a15de37eec5dbfd4
HuydDo <huydinhdo@yahoo.com>
2021-01-13 19:10:09 -0700

    modified RectangleOverlap.js

--- Practice/wordsReverser.js
+++ Practice/wordsReverser.js
@@ -4,7 +4,7 @@
 //     return word.split('').reverse().join('')
 //  }).join(' ');
 // }
-
+// Lockheed Martin question
 const wordsReverser = string => {
   return string.split(' ').map( word => 
     word.split('').reverse().join('')).join(' ')
