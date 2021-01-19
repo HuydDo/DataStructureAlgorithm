c1d8284776d0d41ef5c48713c7c49e29ea5129ac
HuydDo <huydinhdo@yahoo.com>
2021-01-19 16:35:26 -0700

    modified Lemonade Change

--- leetCode/860. Lemonade Change.js
+++ leetCode/860. Lemonade Change.js
@@ -2,6 +2,30 @@
  * @param {number[]} bills
  * @return {boolean}
  */
+
 var lemonadeChange = function(bills) {
-    
+  let five = 0
+  let ten = 0
+  
+  bills.forEach(function(bill){
+      if (bill == 5) 
+          five++
+      else if (bill == 10){
+          if (five == 0) return false
+          five--
+          ten++
+      } else {
+          if (five > 0 && ten > 0){
+              five--
+              ten--
+          } else if (five >= 3){
+              five -=3
+          } else {
+              return false
+          }
+      }
+   
+  })
+  
+  return true   
 };
