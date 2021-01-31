21f3c35c15689d956920210b3b38c46188e6a1d4
HuydDo <huydinhdo@yahoo.com>
2021-01-30 18:59:58 -0700

    deleted 121

--- leetCode/Top 75 LeetCode/Array/121-BestTimetoBuyandSellStock.js
+++ leetCode/Top 75 LeetCode/Array/121-BestTimetoBuyandSellStock.js
@@ -2,16 +2,19 @@
  * @param {number[]} prices
  * @return {number}
  */
-//O(n) solution that does a one pass and continously updates the min price as well
-//as the max difference gained from subtracting the price of the day from the min
+//O(n) solution that does a one pass: updates the min price 
+// and the max difference from subtracting the price of the day from the min
 
  var maxProfit = function(prices) {
   let min = Infinity
   let max = 0
   for(let i = 0; i < prices.length; i++){
+    // find min
     if(prices[i] < min){
       min = prices[i]
-    } else if (prices[i] - min > max){
+    } 
+    // find max difference
+    else if (prices[i] - min > max){
       max = prices[i] - min
     }
   }
