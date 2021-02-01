e167c21a30be0e3782b3cd86f728107d20efa3a0
HuydDo <huydinhdo@yahoo.com>
2021-02-01 11:23:32 -0700

    modified 852 problem

--- leetCode/interviewsSchool/852-PeakIndexinaMountainArray.js
+++ leetCode/interviewsSchool/852-PeakIndexinaMountainArray.js
@@ -3,5 +3,10 @@
  * @return {number}
  */
 var peakIndexInMountainArray = function(arr) {
-    
+    let i = 0;
+    while(arr[i] < arr[i+1]) i++
+    return i
 };
+// array = [0,2,1,0]
+array = [24,69,100,99,79,78,67,36,26,19]
+console.log(peakIndexInMountainArray(array))
