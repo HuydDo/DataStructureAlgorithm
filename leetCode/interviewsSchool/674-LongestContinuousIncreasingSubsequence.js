3892c53f26ccaee70e6ab2619b8b88c665a84c37
HuydDo <huydinhdo@yahoo.com>
2021-02-16 05:22:36 -0700

    modified 674 leet code

--- leetCode/interviewsSchool/674-LongestContinuousIncreasingSubsequence.js
+++ leetCode/interviewsSchool/674-LongestContinuousIncreasingSubsequence.js
@@ -3,5 +3,22 @@
  * @return {number}
  */
 var findLengthOfLCIS = function(nums) {
-    
+  if (nums.length==1) return 1;
+  else {
+        let max=0;
+        for (let i=0;i<nums.length-1;i++) {
+            let count=1;
+            while (i<nums.length-1 && nums[i]<nums[i+1]) {
+                count++;
+                i++;
+            }
+            if (count>max) {
+                max=count;
+            }
+        }
+        return max; 
+  }
 };
+
+let arr = [1,2,5,4,7]
+console.log(findLengthOfLCIS(arr))
