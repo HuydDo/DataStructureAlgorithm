883605a56bbe84036744436a739aa20942884ba1
HuydDo <huydinhdo@yahoo.com>
2021-02-20 20:01:08 -0700

    modified 118 leet code

--- leetCode/interviewsSchool/118-PascalsTriagle.js
+++ leetCode/interviewsSchool/118-PascalsTriagle.js
@@ -11,7 +11,9 @@ var generate = function(numRows) {
   for (var row=0; row < numRows; row++) {
     var arr = []
     arr[0] = arr[row] = 1
-
+    for (var j=1; j<row; j++) {
+      arr[j] = result[row-1][j]+result[row-1][j-1]
+    }
     result[row] = arr
   }
   return result
