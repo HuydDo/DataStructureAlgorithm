ecb20307927221e2bb18a403f17e2543b4f29ac0
HuydDo <huydinhdo@yahoo.com>
2021-03-05 16:33:28 -0700

    added leet problem 303

--- leetCode/interviewsSchool/334-Increasing TripletSubsequence.js
+++ leetCode/interviewsSchool/334-Increasing TripletSubsequence.js
@@ -2,6 +2,23 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canJump = function(nums) {
-    
+var increasingTriplet = function(nums) {
+  var min1 = Number.MAX_VALUE;
+  var min2 = Number.MAX_VALUE;
+  for (var i=0; i<nums.length; i++) {
+      var num = nums[i];
+      if (num<min1) min1 = num;
+      else if (num>min1 && num<min2) min2 = num;
+      else if (num>min2) return true;
+  }
+  return false;
 };
+
+var nums = [1,2,3,4,5];
+console.log(increasingTriplet(nums));
+
+nums = [5,4,3,2,1];
+console.log(increasingTriplet(nums));
+
+nums = [2,1,5,0,4,6];
+console.log(increasingTriplet(nums));
