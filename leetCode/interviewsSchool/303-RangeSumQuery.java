7c39d3255a80205852f059beb174793716adcc7c
HuydDo <huydinhdo@yahoo.com>
2021-03-15 23:59:02 -0600

    modified leet code 53

--- leetCode/interviewsSchool/303-RangeSumQuery.java
+++ leetCode/interviewsSchool/303-RangeSumQuery.java
@@ -20,3 +20,15 @@ public int sumRange(int i, int j) {
 * NumArray obj = new NumArray(nums);
 * int param_1 = obj.sumRange(i,j);
 */
+
+// let a = [5, 7, 8, 9, -1, 3];
+
+// let prefixSum = []
+// prefixSum.length =  a.length;
+// for (let i = 0; i < a.length; i++) {
+//     prefixSum[i] = a[i];
+//     if (i > 0) 
+//         prefixSum[i] += prefixSum[i - 1];
+// }
+
+// console.log(prefixSum)
