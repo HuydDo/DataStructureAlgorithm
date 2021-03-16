b9d73c771b102b3f7ffdc7ed422701774e1bfda9
HuydDo <huydinhdo@yahoo.com>
2021-03-16 11:35:28 -0600

    modified leet code 53

--- leetCode/interviewsSchool/53-MaximumSubarray.js
+++ leetCode/interviewsSchool/53-MaximumSubarray.js
@@ -10,9 +10,14 @@
   var sum = 0
   for (i=0; i<nums.length; i++) {
       sum += nums[i]
+      console.log('sum: ' + sum)
       res = Math.max(res, sum)
       sum = Math.max(sum, 0)
+      console.log('res: ' + res + ' sum: ' + sum)
   }
   return res
 };
+
+let nums = [-2,1,-3,4,-1,2,1,-5,4]
+maxSubArray(nums)
 
