c9e5b125fd84b1cbc8164cd2947477f1c2640c55
HuydDo <huydinhdo@yahoo.com>
2021-03-17 23:16:21 -0600

    modified leet code 238

--- leetCode/interviewsSchool/238-ProductofArrayExceptSelf.js
+++ leetCode/interviewsSchool/238-ProductofArrayExceptSelf.js
@@ -3,5 +3,18 @@
  * @return {number[]}
  */
  var productExceptSelf = function(nums) {
-    
+  var res = []
+  var p=1
+  res.push(1)
+  for (var i=1; i<nums.length; i++) {
+      p*=nums[i-1]
+      res.push(p)
+  }
+
+  p=1
+  for (var i=nums.length-2; i>=0; i--) {
+      p*=nums[i+1]
+      res[i]*=p
+  }
+  return res
 };
