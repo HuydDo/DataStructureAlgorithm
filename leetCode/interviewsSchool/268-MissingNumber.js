c1c361070fb62de30eaff87cd4c227c03c1953f5
HuydDo <huydinhdo@yahoo.com>
2021-03-19 23:37:47 -0600

    added Leet code 268

--- leetCode/interviewsSchool/268-MissingNumber.js
+++ leetCode/interviewsSchool/268-MissingNumber.js
@@ -3,5 +3,12 @@
  * @return {number}
  */
  var missingNumber = function(nums) {
-    
+  if (!nums || nums.length == 0) {
+    return -1
+  }
+  var res = 0
+  for (var i=0; i<nums.length; i++) {
+      res = res ^ nums[i] ^ (i+1)
+  }
+  return res
 };
