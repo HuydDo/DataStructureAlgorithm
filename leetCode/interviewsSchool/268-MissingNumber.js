19b4ec88998b830924dbed6b2d2ccdef9eacc47d
HuydDo <huydinhdo@yahoo.com>
2021-03-20 00:36:33 -0600

    modified Leet code 268

--- leetCode/interviewsSchool/268-MissingNumber.js
+++ leetCode/interviewsSchool/268-MissingNumber.js
@@ -3,14 +3,28 @@
  * @return {number}
  */
  var missingNumber = function(nums) {
-  if (!nums || nums.length == 0) {
-    return -1
+  // if (!nums || nums.length == 0) {
+  //   return -1
+  // }
+  // var res = 0
+  // for (var i=0; i<nums.length; i++) {
+  //     res = res ^ nums[i] ^ (i+1)
+  // }
+  // return res
+
+  // let diff = 0;
+  //       for (let i = 0; i < nums.length; i++) {
+  //           diff += nums[i] - i;
+  //       }
+  //       return nums.length - diff;  
+  
+  let  missing = nums.length;
+  for (let i = 0; i < nums.length; i++) {
+      missing ^= i ^ nums[i];
+      console.log(missing)
+      //3 0 3 = 3 ; 3 1 0 = 0; 0 2 1 = 1
   }
-  var res = 0
-  for (var i=0; i<nums.length; i++) {
-      res = res ^ nums[i] ^ (i+1)
-  }
-  return res
+  return missing;
 };
 
 let nums = [3,0,1]
