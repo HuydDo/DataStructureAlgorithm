c55c2bdb076c63859ad477e22648560ccf9f8f08
HuydDo <huydinhdo@yahoo.com>
2021-03-24 23:35:04 -0600

    added 304 leetCode

--- leetCode/interviewsSchool/304-RangeSumQuery2DImmutable.java
+++ leetCode/interviewsSchool/304-RangeSumQuery2DImmutable.java
@@ -1,17 +1,18 @@
 class NumMatrix {
   private int[][] data;
+ 
   public NumMatrix(int[][] matrix) {
       data = matrix;
   }
   
   public int sumRegion(int row1, int col1, int row2, int col2) {
       int sum = 0;
-  for (int r = row1; r <= row2; r++) {
-      for (int c = col1; c <= col2; c++) {
+      for (int r = row1; r <= row2; r++) {
+        for (int c = col1; c <= col2; c++) {
           sum += data[r][c];
+        }
       }
-  }
-  return sum;
+      return sum;
   }
 }
 
