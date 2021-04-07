f5c49722a6f2e08f404cd6d17ffee064bbb029a4
HuydDo <huydinhdo@yahoo.com>
2021-04-07 13:42:32 -0600

    add graph.js

---
+++ Practice/graph.js
@@ -0,0 +1,42 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {}
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = []
+    }
+  }
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2)
+    this.adjacencyList[vertex2].push(vertex1)
+  }
+  removeEdge(vertex1, vertex2) {
+   this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(neighbor => neighbor !== vertex2)
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(neighbor => neighbor !== vertex1)
+  }
+  removeVertex(vertex) {
+    this.adjacencyList[vertex].forEach(neighbor => this.removeEdge(vertex, neighbor))
+    delete this.adjacencyList[vertex]
+  }
+
+}
+let g = new Graph()
+g.addVertex('jfk')
+g.addVertex('lga')
+g.addVertex('nwk')
+g.addVertex('sjc')
+g.addVertex('ias')
+g.addVertex('sea')
+g.addVertex('mco')
+g.addVertex('atl')
+g.addVertex('yyz')
+g.addEdge('jfk', 'mco')
+g.addEdge('jfk', 'yyz')
+g.addEdge('ias', 'sea')
+g.addEdge('nwk', 'sea')
+g.addEdge('atl', 'mco')
+g.addEdge('sjc', 'sea')
+
+g.removeVertex('mco')
+console.log(g)
