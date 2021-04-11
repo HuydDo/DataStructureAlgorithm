0a8fb510b94bae59e700c634268ac0da95a1ec11
HuydDo <huydinhdo@yahoo.com>
2021-04-10 20:21:37 -0600

    modified 88-MergeSorted

--- leetCode/interviewsSchool/88-MergeSortedArray.js
+++ leetCode/interviewsSchool/88-MergeSortedArray.js
@@ -1,5 +1,39 @@
-class Solution {
-  public void merge(int[] nums1, int m, int[] nums2, int n) {
-      
-  }
-}
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+ var merge = function(nums1, m, nums2, n) {
+  let i = m -1
+   let j = n - 1
+   let k = m + n - 1
+ 
+   while (i >= 0 && j >= 0 ) {
+     if (nums1[i] > nums2[j]) {
+       nums1[k] = nums1[i]
+       k--;
+       i--;
+     } else {
+       nums1[k] = nums2[j] 
+       k--;
+       j--;
+     }
+   }
+   while(j >= 0){
+     console.log(j)
+     nums1[k] = nums2[j];
+     k--;
+     j--;
+     console.log(nums1)
+   }
+   console.log(nums1)
+ }
+
+let nums1 = [1,2,3,0,0,0]  
+let m = 3
+let nums2 = [2,5,6] 
+let  n = 3
+//Output: [1,2,2,3,5,6]
+merge(nums1, m, nums2, n)
