7bb3635ca3fe2e42ab3fc262e15bb4c736bf3be2
HuydDo <huydinhdo@yahoo.com>
2021-04-11 19:07:41 -0600

    updated leetcode 643

--- leetCode/interviewsSchool/643-MaximumAverageSubarrayI.js
+++ leetCode/interviewsSchool/643-MaximumAverageSubarrayI.js
@@ -4,5 +4,21 @@
  * @return {number}
  */
 var findMaxAverage = function(nums, k) {
+    let sum = 0
+    for(let i = 0; i<k; i++){
+      sum += nums[i]
+    }
+    let res = sum
     
+    for(let i= k; i<nums.length; i++){
+      sum += nums[i] - nums[i-k]
+      res = Math.max(res,sum)
+    }
+    return res/k
 };
+
+let nums = [1,12,-5,-6,50,3], k = 4
+//Output: 12.75
+//Explanation: Maximum average is (12-5-6+50)/4 = 51/4 = 12.75
+
+console.log(findMaxAverage(nums,k))
