68ba26d92b60e4f55f9214ff36a291ffdd91c543
HuydDo <huydinhdo@yahoo.com>
2021-04-19 22:40:22 -0600

    modified makeArrayConsecutive2.js

--- codeSignal/Arcade/Intro/makeArrayConsecutive2.js
+++ codeSignal/Arcade/Intro/makeArrayConsecutive2.js
@@ -1,9 +1,11 @@
 function makeArrayConsecutive2(statues) {
   let minS = 0
 	statues.sort((a,b) => b-a)
-	
+	// console.log(statues)
 	for (let i=0; i < statues.length -1; i++){
 			minS += statues[i] - statues[i+1]-1
 	}
   return minS
 }
+let statues = [4,3,5]
+console.log(makeArrayConsecutive2(statues))
