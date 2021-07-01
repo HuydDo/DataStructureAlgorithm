65e0d634c444dc18a61aa9c045f52f489df6439c
HuydDo <huydinhdo@yahoo.com>
2021-07-01 11:41:46 -0600

    completed leetcode 1342

--- NYCCoders/Recursion/1342-numberOfStepsToReduceANumberToZero.js
+++ NYCCoders/Recursion/1342-numberOfStepsToReduceANumberToZero.js
@@ -3,5 +3,19 @@
  * @return {number}
  */
  var numberOfSteps = function(num) {
-    
+  let step = 0;  
+  if (num <= 0){
+    return step 
+  }
+   while(num > 0){  
+
+    if(num % 2 === 0)
+      num = num / 2;
+    else
+      num -= 1;
+    step++;   
+   }
+   return step
 };
+
+console.log(numberOfSteps(8))
