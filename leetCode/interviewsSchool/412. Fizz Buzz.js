3226bcc27676bedda95ec6ff333e427eff3e2712
HuydDo <huydinhdo@yahoo.com>
2021-07-01 11:16:52 -0600

    added another way to print out the result

--- leetCode/interviewsSchool/412. Fizz Buzz.js
+++ leetCode/interviewsSchool/412. Fizz Buzz.js
@@ -2,6 +2,8 @@
  * @param {number} n
  * @return {string[]}
  */
+
+//return array
 var fizzBuzz = function(n) {
   let arr = []
   for(let i= 1; i <= n; i++){ 
@@ -16,6 +18,19 @@ var fizzBuzz = function(n) {
       arr.push(str)
   }
   return arr
+
+
+//print out the string
+// for(let i= 1; i <= n; i++){ 
+//   let str = ""
+  
+//   if((i % 3 == 0) && (i % 5 == 0)) str += "FizzBuzz"
+//   else if (i % 3 == 0) str +="Fizz"
+//   else if(i % 5 == 0) str += "Buzz"
+//   console.log(str|| n);
+// }
+
 };
 
-console.log(fizzBuzz(15))
+fizzBuzz(15)
+
