020b14deb7bf20684bafb5d103e2fcd6497c0831
HuydDo <huydinhdo@yahoo.com>
2021-07-01 13:41:35 -0600

    completed 326 leetcode

---
+++ NYCCoders/Recursion/326-powerOfThree.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+ var isPowerOfThree = function(n) {
+  if (n < 1) {
+    return false; 
+  }
+  
+  while (n % 3 === 0){
+    n /=3;
+  }    
+  
+  return n === 1
+};
+
+console.log(isPowerOfThree(9)) //true
+console.log(isPowerOfThree(45)) //false
